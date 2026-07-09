af0055d5c7909db4e2e1e5453ce602a1a0d23aec
Laércio Bubiak <lbubiak@hotmail.com>
2026-07-09 13:52:01 -0300

    add riad and gregory

--- src/data/team.ts
+++ src/data/team.ts
@@ -16,11 +16,11 @@ export const team: TeamMember[] = [
     photo: "/team/laercio.png",
     linkedinUrl: "https://www.linkedin.com/in/laercio-bubiak/",
     portfolioUrl: "https://www.laercio.me/",
-    tagline: "Automatiza deploys pra ninguém dormir com medo de subir errado.",
+    tagline: "Deixo a conta da Cloud mais barata e automatizo deploys pra ninguém dormir com medo de subir errado.",
     tags: ["Kubernetes", "Docker", "Terraform", "AWS", "CI/CD", "Serverless"," Cloud Security"," Cloud Architecture", "Cloud Cost Optimization"],
   },
   {
-    area: "Frontend",
+    area: "Frontend Engineer",
     lead: "Dionei Bianchati",
     photo: "/team/dionei.png",
     linkedinUrl: "https://www.linkedin.com/in/dionbiancha",
@@ -37,5 +37,24 @@ export const team: TeamMember[] = [
     portfolioUrl: "https://www.viniengelage.com/",
     tagline: "Resolvo bugs para não me estressar e me estresso corrigindo bugs.",
     tags: ["Javascript", "Web", "Mobile", "React", "NextJS", "React Native"," Docker"," Expo"],
+  },
+    {
+    area: "Software Engineer",
+    lead: "Riad Younes",
+    photo: "/team/riad.jpeg",
+    linkedinUrl: "",
+    portfolioUrl: "",
+    tagline: "",
+    tags: ["Automation AI", "Type Script", "Node", "React","Integration API"],
+  },
+
+  {
+    area: "Backend Engineer",
+    lead: "Gregory Xavier Fleury Torres",
+    photo: "/team/gregory.jpeg",
+    linkedinUrl: "",
+    portfolioUrl: "",
+    tagline: "Transformando ideias em algoritmos.",
+    tags: ["MySQL", "Postgresql", "Fast API", "Java", "ORM", "C#","Docker","Integração"],
   },
 ];
